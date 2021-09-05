28125ec7e76d1a9cc4618b2223f60295253f4e23
Pudens Anilus <pudensanilus@pudenss-imac.home>
2021-09-05 15:08:45 -0400

    Adding the electionaudit code.

--- pypoll.py
+++ pypoll.py
@@ -64,11 +64,73 @@ file_to_load = os.path.join("Resources", "election_results.csv")
 # Assign a variable to save the file to a path.
 file_to_save = os.path.join("analysis", "election_analysis.txt")
 
+
+# total vote counter
+total_votes = 0 
+
+
+candidate_options = []
+#empty dictionary
+candidate_votes = {}
+
+# Winning Candidate and Winning Count Tracker
+winning_candidate = ""
+winning_count = 0
+winning_percentage = 0
+
+
 # Open the election results and read the file.
 with open(file_to_load) as election_data:
     file_reader = csv.reader(election_data)
     headers = next(file_reader)
     print(headers)
-    # for row in file_reader:
-    #     #print(row)
+    for row in file_reader:
+        total_votes +=1
+        #print(row)
+
+        candidate_name = row[2]
+
+        if candidate_name not in candidate_options:
+        
+            candidate_options.append(candidate_name)
+
+            candidate_votes[candidate_name] = 0 
+
+        #add a vote to candidate counts
+        candidate_votes[candidate_name] += 1
+
+
+
     
+#print(total_votes)
+
+#print(candidate_options)
+
+print(candidate_votes)
+
+
+for candidate_name in candidate_votes:
+    # 2. Retrieve vote count of a candidate.
+    votes = candidate_votes[candidate_name]
+    # 3. Calculate the percentage of votes.
+    vote_percentage = float(votes) / float(total_votes) * 100
+    # 4. Print the candidate name and percentage of votes.
+    print(f"{candidate_name}: received {vote_percentage}% of the vote.")
+
+
+    if (votes > winning_count) and (vote_percentage > winning_percentage):
+         # If true then set winning_count = votes and winning_percent =
+         # vote_percentage.
+         winning_count = votes
+         winning_percentage = vote_percentage
+         # And, set the winning_candidate equal to the candidate's name.
+         winning_candidate = candidate_name
+    #print(f"{candidate_name}: {vote_percentage:.1f}% ({votes:,})\n")
+    winning_candidate_summary = (
+    f"-------------------------\n"
+    f"Winner: {winning_candidate}\n"
+    f"Winning Vote Count: {winning_count:,}\n"
+    f"Winning Percentage: {winning_percentage:.1f}%\n"
+    f"-------------------------\n")
+print(winning_candidate_summary)
+
